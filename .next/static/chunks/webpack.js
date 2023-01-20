edea9f0eed28f88255d702d02e9ef184082e55c9
rafaelvf <reviterif@gmail.com>
2023-01-20 14:43:11 -0500

    new updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fda08befab8a5cf"; }
+/******/ 		__webpack_require__.h = function() { return "cb024d13bf5bf4c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Wave.tsx
+++ components/Wave.tsx
@@ -1,22 +1,61 @@
 import * as React from "react";
-import { useRef, useState } from "react";
-
+import { useRef, useState, useEffect } from "react";
 import styles from "../styles/Wave.module.scss";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Wave() {
+  const [scrolled, setScrolled] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollProgress);
+  }, []);
+
+  const scrollProgress = () => {
+    const scrollpx = document.documentElement.scrollTop;
+    const winHeightPx =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrollLen = Math.ceil(((scrollpx / winHeightPx) * 100) / 0.69);
+    setScrolled(scrollLen);
+  };
+  //   useEffect(() => {
+  //     const videoContainer = document.querySelector(".containerVideo");
+  //     const videoTag = document.querySelector(".video");
+
+  //     gsap.to(".video", {
+  //       scrollTrigger: {
+  //         trigger: ".video",
+  //         markers: true,
+  //         scrub: true,
+  //         start: "top top",
+  //         end: "99% bottom",
+  //         //pin: true,
+  //       },
+  //     });
+
+  //     //video animation
+  //     let accelamount = 0.1;
+  //     let scrollpos = 0;
+  //     let delay = 0;
+  //   });
   return (
-    <div className={styles.container}>
-      <video
-        width="100%"
-        height="100%"
-        controls={true}
-        autoPlay
+    <div className={`${styles.container} containerVideo`}>
+      {/* <video
+        //autoPlay
+        //muted
+        className={`${styles.video} video`}
         //loop={true}
       >
         <source src="/2.MP4" type="video/mp4" />
-      </video>
+      </video> */}
+      <img
+        src={`/frame_${scrolled.toString()}_delay-1s.png`}
+        className={styles.imgWave}
+      />
     </div>
   );
 }
