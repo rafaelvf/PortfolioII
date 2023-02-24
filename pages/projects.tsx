38f53684c5daf6d850416d48fb74bf740ec9179d
rafaelvf <reviterif@gmail.com>
2023-02-24 14:36:29 -0500

    filtros

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,10 +5,22 @@ export default function ProjectsPage() {
     <div className={styles.container}>
       <div className={styles.title}>Projects</div>
       <div className={styles.filterContainer}>
-        <div className={styles.filter}>All</div>
-        <div className={styles.filter}>Work projects</div>
-        <div className={styles.filter}>Freelance</div>
-        <div className={styles.filter}>Side-Projects</div>
+        <div className={styles.filter}>
+          <div className={styles.filterInner}></div>
+          <a href="#">All</a>
+        </div>
+        <div className={styles.filter}>
+          <div className={styles.filterInner2}></div>
+          <a href="#">Work Projects</a>
+        </div>
+        <div className={styles.filter}>
+          <div className={styles.filterInner3}></div>
+          <a href="#">Side-projects</a>
+        </div>
+        <div className={styles.filter}>
+          <div className={styles.filterInner4}></div>
+          <a href="#">Freelance</a>
+        </div>
       </div>
     </div>
   );
