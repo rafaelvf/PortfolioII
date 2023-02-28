180d2d5368b6ed9c8aebb510e0aafd37098442f6
rafaelvf <reviterif@gmail.com>
2023-02-28 00:09:42 -0500

    buttons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f9369cee8a6cc670"; }
+/******/ 		__webpack_require__.h = function() { return "dceacb11cf7bdaaa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
