575e44e12e45472b441fc9df4fa3b5180d3f0dc5
rafaelvf <reviterif@gmail.com>
2023-02-28 17:03:10 -0500

    d

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,13 +4,13 @@ import { projectsInfo } from "../data";
 import { useState } from "react";
 export default function ProjectsPage() {
   const [filter, setFilter] = useState("All");
+  const [grid, setGrid] = useState(true);
 
   const filteredArray =
     filter === "All"
       ? projectsInfo
       : projectsInfo.filter((i: any) => i.tag === filter);
 
-  console.log(filter, "filter");
   return (
     <div className={styles.container}>
       <div className={styles.title}>Projects</div>
@@ -36,6 +36,34 @@ export default function ProjectsPage() {
         <div className={styles.filter} onClick={() => setFilter("Freelance")}>
           <div className={styles.filterInner4}></div>
           <a href="#">Freelance</a>
+        </div>
+        <div className={styles.filterList}>
+          <div
+            className={
+              grid
+                ? `${styles.iconContainer} ${styles.iconContainerTrue}`
+                : styles.iconContainer
+            }
+            onClick={() => setGrid(true)}
+          >
+            <img
+              src="/grid_view_FILL0_wght400_GRAD0_opsz48.svg"
+              className={styles.icon}
+            />
+          </div>
+          <div
+            className={
+              !grid
+                ? `${styles.iconContainer} ${styles.iconContainerTrue}`
+                : styles.iconContainer
+            }
+            onClick={() => setGrid(false)}
+          >
+            <img
+              src="/list_FILL0_wght400_GRAD0_opsz48.svg"
+              className={styles.icon}
+            />
+          </div>
         </div>
       </div>
       <div className={styles.cardContainer}>
