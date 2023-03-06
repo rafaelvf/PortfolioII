4fc008301b6d36609cb6e8d0fe8e5f41c552d0ce
rafaelvf <reviterif@gmail.com>
2023-03-06 17:07:59 -0500

    d

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cadbe230ec313596"; }
+/******/ 		__webpack_require__.h = function() { return "f45df744510caa1f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -32,19 +32,52 @@ export default function Contact() {
       <div className={`${styles.title} title2`}>Contact</div>
 
       <div className={styles.nav}>
-        <div className={styles.infoContainer}>
-          <img src="/gmail-svgrepo-com.svg" className={styles.logos} />
-        </div>
-        <div className={styles.infoContainer}>
-          <img src="/linkedin-svgrepo-com.svg" className={styles.logos} />
-        </div>
-        <div className={styles.infoContainer}>
-          <img src="/github-svgrepo-com.svg" className={styles.logos} />
-        </div>
-        <div className={styles.infoContainer}>
-          <img src="/twitter-color-svgrepo-com.svg" className={styles.logos} />
-        </div>
-        <div className={styles.infoContainer2}>CV</div>
+        <a
+          href="mailto:reviterif@gmail.com"
+          target="_blank"
+          rel="noopener noreferrerz"
+        >
+          <div className={styles.infoContainer}>
+            <img src="/gmail-svgrepo-com.svg" className={styles.logos} />
+          </div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/rafael-viteri/"
+          target="_blank"
+          rel="noopener noreferrerz"
+        >
+          <div className={styles.infoContainer}>
+            <img src="/linkedin-svgrepo-com.svg" className={styles.logos} />
+          </div>
+        </a>
+        <a
+          href="https://github.com/rafaelvf"
+          target="_blank"
+          rel="noopener noreferrerz"
+        >
+          <div className={styles.infoContainer}>
+            <img src="/github-svgrepo-com.svg" className={styles.logos} />
+          </div>
+        </a>
+        <a
+          href="https://twitter.com/fixes_v"
+          target="_blank"
+          rel="noopener noreferrerz"
+        >
+          <div className={styles.infoContainer}>
+            <img
+              src="/twitter-color-svgrepo-com.svg"
+              className={styles.logos}
+            />
+          </div>
+        </a>
+        <a
+          href="https://www.canva.com/design/DAFcX3I4LLs/AkihSj0essJvQ9eKitRSVg/view?utm_content=DAFcX3I4LLs&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+          target="_blank"
+          rel="noopener noreferrerz"
+        >
+          <div className={styles.infoContainer2}>CV</div>
+        </a>
       </div>
     </div>
   );
