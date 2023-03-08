50ff6fca56654415da855b26c4d36faabf75bb82
rafaelvf <reviterif@gmail.com>
2023-03-07 22:40:12 -0500

    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,28 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import Squares from "../components/Squares";
-import Basketball from "../components/Basketball";
-import Wave from "../components/Wave";
-import WaveReal from "../components/WaveReal";
-import { use, useState } from "react";
+import { use, useState, useRef } from "react";
 import Birds from "../components/Birds";
-import MusicPlayer from "../components/MusicPlayer";
-import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 import ProjectsFramer from "../components/ProjectsFramer";
 import ProjectsMobile from "../components/ProjectsMobile";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [realLife, setRealLife] = useState(false);
+  const ref = useRef(null);
+  const { scrollY, scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+  const opacity = useTransform(scrollY, [100, 200], [0, 1]);
+  const scale = useTransform(scrollY, [0, 100], [1, 0]);
+  const scale2 = useTransform(scrollY, [100, 300], [0, 1]);
+  const width = useTransform(scrollY, [200, 400], [300, 500]);
+  const translate = useTransform(scrollY, [390, 600], [0, -500]);
+
   return (
     <>
       <Head>
@@ -27,7 +32,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main ref={ref} className={styles.main}>
         {/* <Squares /> */}
         {/* <Basketball /> */}
         {/* <div
@@ -40,27 +45,33 @@ export default function Home() {
 
         <div className={styles.heroContainer}>
           <Birds />
-
-          <video
-            muted
-            playsInline
-            autoPlay
-            preload="auto"
-            loop
-            className={styles.title}
-          >
-            <source src="/video2.mp4" type="video/mp4" />
-          </video>
+          <div className={styles.videoContainer}>
+            <motion.video
+              style={{ scale: scale }}
+              muted
+              playsInline
+              autoPlay
+              preload="auto"
+              loop
+              className={styles.title}
+            >
+              <source src="/video2.mp4" type="video/mp4" />
+            </motion.video>
+          </div>
           <img
             src="/expand_more_FILL0_wght400_GRAD0_opsz48.svg"
             className={styles.arrow}
           />
-          {/* <div className={styles.text}>
-            I'm a developer who loves to create good user experiences. My goal
-            is to build innovative digital products that have a positive impact
-            on society.{" "}
-          </div> */}
-
+          <div className={styles.textContainer}>
+            <motion.div
+              style={{ opacity, scale: scale2 }}
+              className={styles.text}
+            >
+              I'm a developer who loves to create good user experiences. My goal
+              is to build innovative digital products that have a positive
+              impact on society.{" "}
+            </motion.div>
+          </div>
           {/* <div className={styles.title}>FIXES</div> */}
           {/* <MusicPlayer /> */}
         </div>
